f882bea6d5a1c156346df8bcc0195315cce87caa
oktavianidewi <oktaviani.dewi@gmail.com>
2023-04-05 11:00:47 +0700

    Fix install tf

---
+++ install-terraform.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg && \
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list && \
+sudo apt-get update -y && \
+sudo apt install terraform -y
